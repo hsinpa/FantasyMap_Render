925f44a3b660764045fe83199096689349bdfe62
Hsinpa <hsinpa@gmail.com>
2024-03-27 13:50:12 +0800

    Add data type

--- Assets/Hsinpa/Script/FantasyMap/FantasyMapDatatype.cs
+++ Assets/Hsinpa/Script/FantasyMap/FantasyMapDatatype.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 public struct FM_Vertices_Type {
     /// <summary>
     /// Index
@@ -31,4 +33,79 @@ public struct FM_Cells_Type {
     public uint[] c;
 
     public uint area;
+}
+
+public struct FM_Burg_Type {
+    public uint i;
+
+    public uint state;
+
+    // Index of cell
+    public uint cell;
+
+    public uint burg;
+
+    public string name;
+
+    public float x;
+    public float y;
+    public float population;
+}
+
+
+public struct FM_Province_Type {
+    public uint i;
+
+    public uint state;
+
+    // Index of cell
+    public uint center;
+
+    public uint burg;
+
+    public string name;
+    public string formName;
+    public string fullName;
+
+    public string color;
+}
+
+public struct FM_State_Type {
+    public uint i;
+
+    public string color;
+
+    public string name;
+    public string formName;
+    public string fullName;
+
+    //
+    public uint capital;
+
+    //
+    public string type;
+
+    // cell id of state center (initial cell)
+    public uint center;
+
+    // state culture id (equals to initial cell culture)
+    public uint culture;
+
+    public float urban;
+    public float rural;
+
+    // number of burgs within the state
+    public int burgs;
+
+    // state area in pixels
+    public int area;
+
+    // number of cells within the state
+    public int cells;
+
+    //ids of neighboring (bordering by land) states
+    public List<uint> neighbors;
+
+    //ids of state provinces
+    public List<uint> provinces;
 }
