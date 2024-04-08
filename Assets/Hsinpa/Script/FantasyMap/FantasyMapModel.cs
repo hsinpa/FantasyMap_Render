a6f8c75cfd99f3341ad3063044b8f864a5a05a72
Hsinpa <hsinpa@gmail.com>
2024-04-08 20:48:28 +0800

    Add state and province data

--- Assets/Hsinpa/Script/FantasyMap/FantasyMapModel.cs
+++ Assets/Hsinpa/Script/FantasyMap/FantasyMapModel.cs
@@ -11,6 +11,8 @@ namespace Hsinpa.Map
     {
         public Dictionary<uint, FM_Vertices_Type> vertices_dict = new Dictionary<uint, FM_Vertices_Type> ();
         public Dictionary<uint, FM_Cells_Type> cells_dict = new Dictionary<uint, FM_Cells_Type>();
+        public Dictionary<uint, FM_Province_Type> provinces_dict = new Dictionary<uint, FM_Province_Type>();
+        public Dictionary<uint, FM_State_Type> states_dict = new Dictionary<uint, FM_State_Type>();
 
         public async Task Load(string file_path)
         {
@@ -21,9 +23,14 @@ namespace Hsinpa.Map
 
                 var cells = simple_json["cells"]["cells"].AsArray;
                 var vertices = simple_json["cells"]["vertices"].AsArray;
+                var provinces = simple_json["cells"]["provinces"].AsArray;
+                var states = simple_json["cells"]["states"].AsArray;
 
                 cells_dict = Load_Map_Cells(cells);
                 vertices_dict = Load_Map_Vertices(vertices);
+                provinces_dict = Load_Map_Province(provinces);
+                states_dict = Load_Map_State(states);
+
 
                 Debug.Log("Simple json readed");
             });
@@ -56,6 +63,37 @@ namespace Hsinpa.Map
             }
 
             return cell_dict;
+        }
+
+        private Dictionary<uint, FM_Province_Type> Load_Map_Province(JSONArray provinces_array)
+        {
+            Dictionary<uint, FM_Province_Type> province_dict = new Dictionary<uint, FM_Province_Type>();
+
+            for (int i = 0; i < provinces_array.Count; i++)
+            {
+                string raw_json = provinces_array[i].ToString();
+
+                if (!raw_json.StartsWith("{")) continue;
+
+                FM_Province_Type province = JsonUtility.FromJson<FM_Province_Type>(raw_json);
+
+                province_dict.Add(province.i, province);
+            }
+
+            return province_dict;
+        }
+
+        private Dictionary<uint, FM_State_Type> Load_Map_State(JSONArray states_array)
+        {
+            Dictionary<uint, FM_State_Type> states_dict = new Dictionary<uint, FM_State_Type>();
+
+            for (int i = 0; i < states_array.Count; i++)
+            {
+                string raw_json = states_array[i].ToString();
+                Debug.Log(raw_json);
+            }
+
+            return states_dict;
         }
 
     }
